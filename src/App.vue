bf5401701743a1a1cfa21fb3f0bf268552dcd418
smallbore <291531083@qq.com>
2018-02-23 14:18:53 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app">
+    hello world
+  </div>
+</template>
+
+<style socoped lang="stylus" rel="stylesheet/stylus">
+  @import '~common/stylus/variable'
+
+  #app
+    color: $color-theme
+</style>
